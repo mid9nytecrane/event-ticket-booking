bc2fc35e244af8addbd351e513e04377e2cbfea8
mid9nytecrane <sheriffsakara41@gmail.com>
2026-01-01 06:53:02 +0000

    commit

--- Event_Booking/settings.py
+++ Event_Booking/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG",default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+
 
 if DEBUG:
     ALLOWED_HOSTS =['localhost', '127.0.0.1']
