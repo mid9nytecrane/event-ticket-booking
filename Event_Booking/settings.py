1855df0572e58765676e2917aee5273685748cef
mid9nytecrane <sheriffsakara41@gmail.com>
2026-01-01 07:56:39 +0000

    modified settings.py

--- Event_Booking/settings.py
+++ Event_Booking/settings.py
@@ -27,9 +27,11 @@ SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = config("DEBUG", cast=bool)
-DEBUG = True
-ALLOWED_HOSTS = ['reventtribe-cv8c.onrender.com']
-ALLOWED_HOSTS = ['*']
+DEBUG = False
+if DEBUG == True:
+    ALLOWED_HOSTS = ['*']
+else:
+    ALLOWED_HOSTS = ['reventtribe-cv8c.onrender.com']
 
 #ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
