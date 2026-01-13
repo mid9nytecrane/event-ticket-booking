9d8a526af43442f3af07c79fddde95bb961af52d
mid9nytecrane <sheriffsakara41@gmail.com>
2026-01-13 13:35:45 +0000

    added Procfile

--- Event_Booking/settings.py
+++ Event_Booking/settings.py
@@ -250,7 +250,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-if config('ENVIRONMENT') == 'development':
+if config('ENVIRONMENT') != 'development':
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
